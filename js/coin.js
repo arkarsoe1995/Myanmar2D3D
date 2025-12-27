69e429b82bfeaa02ef041fa5cbfed9dff8be0a67
arkarsoe1995 <arkarsoe.mtk@gmail.com>
2025-12-27 21:39:55 +0630

    Update coin.js

--- js/coin.js
+++ js/coin.js
@@ -1,7 +1,4 @@
-// Load or initialize users array
 let users = JSON.parse(localStorage.getItem("users")) || [];
-
-// Current user
 let user = JSON.parse(localStorage.getItem("user"));
 if (!user) location.href = "login.html";
 
@@ -9,19 +6,16 @@ document.getElementById("userName").innerText = user.name;
 document.getElementById("userCoin").innerText = "💰 " + user.coin;
 
 function updateCoin() {
-  // Update current user in users array
   let idx = users.findIndex(u => u.name === user.name);
-  if (idx !== -1) {
-    users[idx] = user;
-  } else {
-    users.push(user);
-  }
+  if (idx !== -1) users[idx] = user;
+  else users.push(user);
+
   localStorage.setItem("user", JSON.stringify(user));
   localStorage.setItem("users", JSON.stringify(users));
   document.getElementById("userCoin").innerText = "💰 " + user.coin;
 }
 
-// Ads simulate
+// Watch Ads simulate
 function watchAds() {
   alert("Ads watched! +5000 Coin");
   user.coin += 5000;
