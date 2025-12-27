a0adaef674a76c0efafa155b8e37e9f8b56125a3
arkarsoe1995 <arkarsoe.mtk@gmail.com>
2025-12-27 21:38:08 +0630

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -3,12 +3,17 @@ function register() {
   let pass = document.getElementById("regPass").value;
   if (!name || !pass) return alert("Fill all fields");
 
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+  if(users.find(u => u.name === name)) return alert("Username already exists");
+
   let user = {
     name: name,
     pass: pass,
     coin: 5000,
     history: []
   };
+  users.push(user);
+  localStorage.setItem("users", JSON.stringify(users));
   localStorage.setItem("user", JSON.stringify(user));
   alert("Register success! Free 5000 coin 🎉");
   window.location.href = "home.html";
@@ -18,10 +23,11 @@ function login() {
   let name = document.getElementById("loginName").value;
   let pass = document.getElementById("loginPass").value;
 
-  let user = JSON.parse(localStorage.getItem("user"));
-  if (!user || user.name !== name || user.pass !== pass) {
-    return alert("Wrong username or password");
-  }
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+  let user = users.find(u => u.name === name && u.pass === pass);
+  if(!user) return alert("Wrong username or password");
+
+  localStorage.setItem("user", JSON.stringify(user));
   alert("Login success");
   window.location.href = "home.html";
 }
