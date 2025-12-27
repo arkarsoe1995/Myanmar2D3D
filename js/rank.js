34c135cb1aa26854552d616e7b3997a6aef69b76
arkarsoe1995 <arkarsoe.mtk@gmail.com>
2025-12-27 21:40:58 +0630

    Update rank.js

--- js/rank.js
+++ js/rank.js
@@ -1,13 +1,16 @@
 function showRank() {
   const rankList = document.getElementById("rankList");
   let users = JSON.parse(localStorage.getItem("users")) || [];
-  // sort by coin descending
   users.sort((a,b) => b.coin - a.coin);
 
   rankList.innerHTML = "";
   users.forEach((u,i) => {
     let div = document.createElement("div");
     div.innerText = `${i+1}. ${u.name} - 💰 ${u.coin}`;
+    div.style.padding = "8px 12px";
+    div.style.background = "#1e1e1e";
+    div.style.margin = "6px 0";
+    div.style.borderRadius = "10px";
     rankList.appendChild(div);
   });
 }
