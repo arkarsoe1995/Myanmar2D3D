6c6ec786478e219ec98cdef689bb73e593ed1dcd
arkarsoe1995 <arkarsoe.mtk@gmail.com>
2025-12-27 20:47:54 +0630

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,35 +1,27 @@
 function register() {
-  let name = regName.value;
-  let pass = regPass.value;
-
-  if (!name || !pass) {
-    alert("Fill all fields");
-    return;
-  }
+  let name = document.getElementById("regName").value;
+  let pass = document.getElementById("regPass").value;
+  if (!name || !pass) return alert("Fill all fields");
 
   let user = {
     name: name,
     pass: pass,
-    coin: 5000,   // 🎁 Free Coin
+    coin: 5000,
     history: []
   };
-
   localStorage.setItem("user", JSON.stringify(user));
   alert("Register success! Free 5000 coin 🎉");
   window.location.href = "home.html";
 }
 
 function login() {
-  let name = loginName.value;
-  let pass = loginPass.value;
+  let name = document.getElementById("loginName").value;
+  let pass = document.getElementById("loginPass").value;
 
   let user = JSON.parse(localStorage.getItem("user"));
-
   if (!user || user.name !== name || user.pass !== pass) {
-    alert("Wrong username or password");
-    return;
+    return alert("Wrong username or password");
   }
-
   alert("Login success");
   window.location.href = "home.html";
 }
