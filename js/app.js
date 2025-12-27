b1bcb34dc675c0e9bf5d6a8ccc8b647bd337ca2e
arkarsoe1995 <arkarsoe.mtk@gmail.com>
2025-12-27 21:34:50 +0630

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 setTimeout(() => {
   let user = localStorage.getItem("user");
-  if (user) {
-    window.location.href = "home.html";
-  } else {
-    window.location.href = "login.html";
-  }
+  if (user) window.location.href = "home.html";
+  else window.location.href = "login.html";
 }, 2000);
