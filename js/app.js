dbdb37a1397c861cb5f124cc16ba829b8228835d
arkarsoe1995 <arkarsoe.mtk@gmail.com>
2025-12-27 19:42:19 +0630

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+  let user = localStorage.getItem("user");
+
+  if (user) {
+    window.location.href = "home.html";
+  } else {
+    window.location.href = "login.html";
+  }
+}, 2000);
